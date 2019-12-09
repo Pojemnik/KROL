b10081f6e5862692c58ad384964dc620945c4842
Pojemnik <31095662+Pojemnik@users.noreply.github.com>
2019-12-09 19:15:33 +0100

    Poprawienie literówek i kodowania

--- interrupt_led.c
+++ interrupt_led.c
@@ -4,15 +4,15 @@
 
 /*
 interrupt_led.c
-LEDy pod³¹czony do pinu PD5
+LED podÅ‚Ä…czony do pinu PD5
 Przycisk na pinie PD2 (INT0) - pullup
-KRÓL 2019
+KRÃ“L 2019
 */
 
 int main(void)
 {
-	EICRA = _BV(ISC01);	//Przerwanie na zbpczu opadaj¹cym
-	EIMSK = _BV(INT0);	//W³¹cz przerwanie INT0 (PD2)
+	EICRA = _BV(ISC01);	//Przerwanie na zboczu opadajÄ…cym
+	EIMSK = _BV(INT0);	//WÅ‚Ä…cz przerwanie INT0 (PD2)
 	DDRD = 0b11111011;
 	PORTD = 0;
 	sei();
@@ -23,7 +23,7 @@ int main(void)
 	}
 }
 
-ISR(INT0_vect)			//Obs³uga przerwania INT0
+ISR(INT0_vect)			//ObsÅ‚uga przerwania INT0
 {
 	PIND = _BV(PD5);
 	_delay_ms(2000);

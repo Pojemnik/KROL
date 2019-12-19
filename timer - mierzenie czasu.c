7742fdbda32fe0cce8bad8052674a2843bcda6c8
Karanami <piotrfinic@gmail.com>
2019-12-19 22:48:03 +0100

    Update timer - mierzenie czasu.c

--- timer - mierzenie czasu.c
+++ timer - mierzenie czasu.c
@@ -1,27 +1,27 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-uint8_t led_2 = 0;
+//* program
+//  sprawia ze dwie diody zapalaja sie i gasna za pomoca timer 
+
+//* podlaczenie 
+//  dwa ledy jednen do PC5 drugi do PC4
 
 volatile uint8_t var_time;
 
-
-
 int main(void)
 {
-	DDRC = 0xFF; // ustawienie wszystkich portów na wyjscie 
+	DDRC = 0xFF; // ustawienie wszystkich portÃ³w na wyjscie 
 	
 	TCCR0A = (1 << WGM01) | (1 << COM0A1); // ustawienie trubu zegara na CTC (Clear Timer on Comper)
 	TCCR0B = (1 << CS02) | (1 << CS00); // prescaler 1024
 	
-	OCR0A = 78; // troche matmy ---> 8 000 000 Hz / 1024 = 7 812,5 Hz (zegar dla timer - 7812,5 cykli na sekunde) ---> 78,125 cykli na 0,01s
-	// przerwanie wykona siê raz na ok 0,01s
+	OCR0A = 78; // troche matmy ---> 8 000 000 Hz / 1024 = 7Â 812,5 Hz (zegar dla timer - 7812,5 cykli na sekunde) ---> 78,125 cykli na 0,01s
+	// przerwanie wykona siÃª raz na ok 0,01s
 	
 	TIMSK0 |= (1 << OCIE0A);                // zezwolenie na przerwanie CompareMatch
 	
 	sei();                              //zezwolenie na prerwania globalne
-	
-	//PORTC |= (1 << PC);
 	
     while (1) 
     {
